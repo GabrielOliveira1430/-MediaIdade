481e54af8ddf06afde7da0c24111a2cddc6aaf96
Gabriel OLiveira <gabrielerika8724@gmail.com>
2025-08-21 21:47:22 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let evento = 'Jogar video Game';
+
+for(let g =1; g <= 30; g++){
+
+    this.adicionarEvento(g, evento)
+
+}
+
+
+function adicionarEvento(dia, evento){
+    console.log('Quero jogar ' +evento+ ' nos dias ' +dia+ '.')
+}
