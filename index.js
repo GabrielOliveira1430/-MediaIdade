3d568e9ca29c80a437da44f88043048a704f6271
Gabriel OLiveira <gabrielerika8724@gmail.com>
2025-08-22 16:38:02 -0300

    função Buzz

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ for(let g =1; g <= 30; g++){
 function adicionarEvento(dia, evento){
     console.log('Quero jogar ' +evento+ ' nos dias ' +dia+ '.')
 }
+
+function MaiorNumero(numero1, numero2){
+    if(numero1 > numero2){
+        return numero1
+    }else{
+        return numero2
+    }
+}
+console.log(MaiorNumero(2,4))
+
+
+function buzzFizz(input){
+    if(typeof input  == 'string')return 'Not number'
+    if(input % 3 == 0 && input % 5 == 0) return 'buzzFizz'
+    if(input % 3 == 0) return 'buzz'
+    if(input % 5 == 0) return 'fizz'
+    else return input
+}
+
+console.log(buzzFizz(3))
